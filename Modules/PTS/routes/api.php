8e66a812bfa11df3c5282235b126d3b90b8f82bf
SHOKRYsh <shokrymansor123@Gmail.com>
2025-11-06 16:12:07 +0200

    Get the data from pioneer and store in mongo :)

--- Modules/PTS/routes/api.php
+++ Modules/PTS/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\PTS\Http\Controllers\PTSController;
 
-Route::prefix('pts')->group(function () {
+Route::middleware(['auth:sanctum','role:Admin'])->prefix('pts')->group(function () {
     Route::get('/fetch', [PTSController::class, 'fetchAndStorePTSData']);
     Route::get('/data', [PTSController::class, 'getStoredPTSData']);
 });
